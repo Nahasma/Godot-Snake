957c0540a09ee92df8de6441705373e8f71bdd26
Nahasma <philiranto@gmail.com>
2023-11-26 04:25:07 +0800

    UI-update1.2
    1. 创建了Complete界面
    
    2. 发现了初始化时收集物有一定概率生成在障碍物上的BUG（与Apple初始化机制有关系）

--- Scene/UI/Complete/Complete.gd
+++ Scene/UI/Complete/Complete.gd
@@ -3,6 +3,8 @@ extends Control
 func _on_Try_pressed():
 	Global.go_to_world("World1")
 
-
 func _on_Home_pressed():
 	Global.go_to_ui("Title")
+
+func _on_Button_pressed():
+	Global.go_to_world("Next")
